de8f09c2c3f4438202cca97f03390ec5a6e5a709
Ignacio Gormaz <ignacio.gormaz199@gmail.com>
2019-05-11 01:25:15 -0400

    tarea3

---
+++ vector_ompi/main.cpp
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: ignacio
+ *
+ * Created on 11 de mayo d
